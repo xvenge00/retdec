f6eaa9c7d167652ba9d4c81c6aa5fb123ce8753d
Michal Bandzi <michal.bandzi@avast.com>
2018-02-14 16:32:02 +0100

    cpdetect: turn off detections for all architectures in fat binary

--- src/cpdetect/compiler_detector/macho_compiler.cpp
+++ src/cpdetect/compiler_detector/macho_compiler.cpp
@@ -27,58 +27,45 @@ MachOCompiler::MachOCompiler(
 	path.append(YARA_RULES_PATH + "macho/");
 	auto bitWidth = parser.getWordLength();
 
-	if (parser.isFatBinary())
+	switch(targetArchitecture)
 	{
-		for (auto it = path.begin(), end = path.end(); it != end; it++)
-		{
-			if (it->isFile())
+		case Architecture::X86:
+			path.append("x86.yarac");
+			break;
+
+		case Architecture::X86_64:
+			path.append("x64.yarac");
+			break;
+
+		case Architecture::ARM:
+			if (bitWidth == 32)
 			{
-				internalPaths.emplace_back(it->getPath());
+				path.append("arm.yarac");
 			}
-		}
+			else
+			{
+				// There are no 64-bit ARM signatures for now.
+			}
+			break;
+
+		case Architecture::POWERPC:
+			if (bitWidth == 32)
+			{
+				path.append("ppc.yarac");
+			}
+			else
+			{
+				path.append("ppc64.yarac");
+			}
+			break;
+
+		default:
+			break;
 	}
-	else
+
+	if (path.isFile())
 	{
-		switch(targetArchitecture)
-		{
-			case Architecture::X86:
-				path.append("x86.yarac");
-				break;
-
-			case Architecture::X86_64:
-				path.append("x64.yarac");
-				break;
-
-			case Architecture::ARM:
-				if (bitWidth == 32)
-				{
-					path.append("arm.yarac");
-				}
-				else
-				{
-					// There are no 64-bit ARM signatures for now.
-				}
-				break;
-
-			case Architecture::POWERPC:
-				if (bitWidth == 32)
-				{
-					path.append("ppc.yarac");
-				}
-				else
-				{
-					path.append("ppc64.yarac");
-				}
-				break;
-
-			default:
-				break;
-		}
-
-		if (path.isFile())
-		{
-			internalPaths.emplace_back(path.getPath());
-		}
+		internalPaths.emplace_back(path.getPath());
 	}
 }
 
